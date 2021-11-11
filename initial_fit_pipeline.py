3294322af75694b8ca378d4f2cef222c962aa542
Brendan Case <brendan.case@uvm.edu>
2021-11-11 14:16:59 -0500

    county active now start at date 1

--- initial_fit_pipeline.py
+++ initial_fit_pipeline.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy.stats
-import pystan
+# import pystan
 from data_utils import *
 import seaborn as sns
-import pickle
+# import pickle
 
-jhu_data = load_jh_data(saveto='./cached_data/JHU_11-09-21.csv')
+jhu_data = load_jh_data(cached_fname='./cached_data/JHU_11-11-21.csv')
 jhu_california = pd.DataFrame(select_state_JHU(jhu_data, 'California'))
 start_date_col = jhu_california.columns.get_loc('5/1/20')
 end_date_col = jhu_california.columns.get_loc('5/1/21')
@@ -16,7 +16,7 @@ end_date_col = jhu_california.columns.get_loc('5/1/21')
 
 
 sample_facility = 'CALIFORNIA STATE PRISON LOS ANGELES'
-sample_covid_data = load_ucla_data(cached_fname='./cached_data/UCLA_11-02-21.csv')
+sample_covid_data = load_ucla_data(cached_fname='./cached_data/UCLA_11-11-21.csv')
 # la_county = pd.DataFrame(select_county(sample_covid_data, 'LOS ANGELES', 'California'))
 la_county = pd.DataFrame(select_county(sample_covid_data, None, 'California'))
 for facility in set(la_county['Name'].values):
@@ -40,25 +40,30 @@ for facility in set(la_county['Name'].values):
         combined_df = combined_df.rename(columns={int(combined_df.columns[6]): "County.cumul"})
         combined_df['County.Pop'] = [county_pop for i in range(len(combined_df))]
         combined_df['County'] = [county_name_lower for i in range(len(combined_df))]
-        combined_df.to_csv(f'./joined_datasets_california/{county_name_lower.replace(" ", "-")}_{fac_ID}.csv')
+        combined_df["County.new"] = combined_df["County.cumul"].diff()/county_pop
+        combined_df['County.active'] = combined_df['County.cumul'].diff(periods=14)/county_pop
+        combined_df['County.active'] = combined_df['County.active'].mask(pd.isnull, (combined_df['County.cumul'] - combined_df['County.cumul'].iloc[0]) / county_pop)
+
+        combined_df.to_csv(f'./joined_datasets_california2/{county_name_lower.replace(" ", "-")}_{fac_ID}.csv')
 
         fig, axs = plt.subplots(2, 1)
         axs[0].scatter(combined_df['Date'], combined_df['Residents.Active']/combined_df['Residents.Population'], label=facility, s=2)
         axs[0].legend()
-        axs[1].scatter(combined_df['Date'], combined_df[combined_df.columns[6]].diff()/county_pop, label=f'{county_name_lower} daily new', s=2)
-        axs[1].scatter(combined_df['Date'], combined_df[combined_df.columns[6]].diff(periods=14)/county_pop, label=f'{county_name_lower} 14-day diff', s=2)
+        axs[1].scatter(combined_df['Date'], combined_df['County.new'], label=f'{county_name_lower} daily new', s=2)
+        axs[1].scatter(combined_df['Date'], combined_df['County.active'], label=f'{county_name_lower} 14-day diff', s=2)
         axs[1].set_ylim([-.01, .09])
         plt.legend()
         plt.ylabel('Percent of population')
-        plt.savefig(f'./jointplots/{facility.replace(" ", "-")}_may20_may21_jhu.png')
+        plt.savefig(f'./jointplots2/{county_name_lower.replace(" ", "-")}_{fac_ID}.png')
         plt.show()
     except:
+        print(facility)
         continue
     # plt.scatter(la_metro[['Date']], la_metro[['Residents.Confirmed']])
-plt.show()
+# plt.show()
 
-la_metro = pd.DataFrame(select_facility(la_county, sample_facility))
-la_metro = pd.DataFrame(select_date_range_UCLA(la_metro, "05-01-2020", "02-01-2021"))
+# la_metro = pd.DataFrame(select_facility(la_county, sample_facility))
+# la_metro = pd.DataFrame(select_date_range_UCLA(la_metro, "05-01-2020", "02-01-2021"))
 # plt.scatter(la_metro[['Date']], la_metro[['Residents.Active']], label=sample_facility)
 # plt.scatter(la_metro[['Date']], la_metro[['Residents.TAdmin']])
 # plt.legend()
@@ -67,24 +72,23 @@ la_metro = pd.DataFrame(select_date_range_UCLA(la_metro, "05-01-2020", "02-01-20
 # sns.pairplot(data=[np.arange(5), np.arange(5)])
 # plt.show()
 
-cases = la_metro[['Residents.Active']]
-cases = cases.dropna()
-cases.to_csv('sample_facility_cases.csv')
-x_days = np.arange(1, len(cases)) ## [0, 1, 2, 3, 4, 5...]
+# cases = la_metro[['Residents.Active']]
+# cases = cases.dropna()
+# cases.to_csv('sample_facility_cases.csv')
+# x_days = np.arange(1, len(cases)) ## [0, 1, 2, 3, 4, 5...]
 
-free_model = pystan.StanModel(file='stan-scripts/fit-sir-free.stan'
-                              , include_paths=['./stan-scripts/'])
-fit = free_model.sampling(chains=1,
-                          data={
-                              'max_t':len(cases),
-                              'ts': x_days,
-                              'y': cases,
-                              'inf_init': max(cases[1],1),
-                              'alpha': 0.1
-                          })
-print(fit)
+# free_model = pystan.StanModel(file='stan-scripts/fit-sir-free.stan'
+#                               , include_paths=['./stan-scripts/'])
+# fit = free_model.sampling(chains=1,
+#                           data={
+#                               'max_t':len(cases),
+#                               'ts': x_days,
+#                               'y': cases,
+#                               'inf_init': max(cases[1],1),
+#                               'alpha': 0.1
+#                           })
+# print(fit)
 
-pickle.dump(fit, open('fit-sir-free.pkl', 'wb'))
-
+# pickle.dump(fit, open('fit-sir-free.pkl', 'wb'))
 
 print('done')
